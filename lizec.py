0b4dd63fc065c9a540dc22af0889fc3b8a44446b
DataZac <jonastrautmann@gmx.de>
2018-02-25 15:47:57 +0100

    Initial commit

---
+++ lizec.py
@@ -0,0 +1,11 @@
+def vivec(eigenschaft):
+	""" output one of vivecs attributes along with a statement from vivec!
+
+	Args:
+
+	Returns:
+
+	"""
+	print('I am vivec, the {} and i live in a town called vivec as well.'.format(eigenschaft))
+
+    
